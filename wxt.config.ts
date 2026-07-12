37ffb24d0903c390dfa4a1a465d574385dac7088
Rahil Hamza <rahilhamza786@gmail.com>
2026-07-12 23:08:00 +0530

    Initial commit: Reddit Assistant WXT Chrome Extension

---
+++ wxt.config.ts
@@ -0,0 +1,16 @@
+import { defineConfig } from "wxt";
+
+// See https://wxt.dev/api/config.html
+export default defineConfig({
+  modules: ["@wxt-dev/module-react"],
+
+  manifest: ({ browser, manifestVersion, mode, command }) => {
+    return {
+      manifest_version: 2,
+      name: "WXT Example",
+      description: "WXT Example",
+      version: "1.0.0",
+      permissions: ["storage", "tabs"],
+    };
+  },
+});
